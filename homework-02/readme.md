8eb2756e4bc5caa1375e6c4dbd71adb117c5c0b0
Taylor Miller <89813359+TaylorMiller-gh@users.noreply.github.com>
2022-01-31 11:41:39 -0700

    Week 2 completed

---
+++ homework-02/readme.md
@@ -0,0 +1,3 @@
+## **Week 2** ##
+
+I was familiar with most of the content we covered this week. Some of the console material from Codecademy was new, though. I had never seen template literal and it is surely the best thing to ever happen to concatenation. I had a lot of fun going through the Codecademy course - I'll take any opportunity I can to practice the fundamentals of a language.  
---
+++ homework-02/script.js
@@ -0,0 +1,24 @@
+//Favorite websites
+let website0 = 'Youtube';
+let website1 = 'Twitch';
+let website2 = 'trex-runner.com';
+
+//Favorite Games
+let game0 = 'Zelda';
+let game1 = 'Minecraft';
+let game2 = 'Apex Legends';
+
+//Favorite Artists
+let artist0 = 'Salvador Dali';
+let artist1 = 'Michael Cheval';
+let artist2 = 'Yves Tanguy';
+
+/* Template literal is the newest thing I have learned
+  so I have decided to use it in excess.
+*/
+
+console.log(`My favorite websites are ${website0}, ${website1}, and ${website2}.`);
+console.log(`My favorite games are ${game0}, ${game1}, and ${game2}.`);
+console.log(`My favorite artists are ${artist0}, ${artist1}, and ${artist2}.`);
+console.log('These artists played major roles in the Absurdist/Surrealist movements.');
+console.log('Their work influenced the post-modern movement and helped the people cultivate freedom.');
